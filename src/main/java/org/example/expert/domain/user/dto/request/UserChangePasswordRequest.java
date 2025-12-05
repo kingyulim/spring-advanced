745cc13cfad6e9bb70b19aae3182ecfc9ee90497
Kingyul Kim <youremail@example.com>
2025-12-05 11:34:27 +0900

    ♻ refactor: 비밀번호 변경 부분 Validation 수정 UserService에서 changePassword안에서 처리 하던 비밀번호 정규식을 UserChangePasswordRequest 안에서 처리 하도록 변경

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.{8,}$)(?=.*\\d)(?=.*[A-Z])\\S+$",
+            message = "비밀번호는 공백 없이 8자 이상이어야 하며 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
